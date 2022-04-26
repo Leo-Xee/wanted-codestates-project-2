aa15ce0f41238b3a675c1483144d90f0abe68527
Leo-Xee <jang.xmin@gmail.com>
2022-04-26 17:09:49 +0900

    Feat: MatchList 컴포넌트 추가

---
+++ src/components/MatchList/MatchList.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+import { SOLO } from "@/constant/matchType";
+import { UserInfo } from "api";
+import Match from "./MatchItem";
+import * as S from "./style";
+
+type MatchListProps = {
+  user: UserInfo;
+  matchType: string;
+};
+
+function MatchList({ user, matchType }: MatchListProps) {
+  const { matchList } = matchType === SOLO ? user.solo : user.team;
+
+  return (
+    <S.Container>
+      {matchList.map((match, idx) => (
+        <Match key={idx} match={match} />
+      ))}
+    </S.Container>
+  );
+}
+
+export default MatchList;
---
+++ src/components/MatchList/style.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1100px;
+  height: 100%;
+  margin: 0 auto;
+  margin-top: 20px;
+`;
