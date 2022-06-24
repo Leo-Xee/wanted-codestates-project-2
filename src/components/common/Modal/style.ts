d18b4faf48bf8512bde69b3bc11c077a42b83255
Leo-Xee <jang.xmin@gmail.com>
2022-06-25 00:20:43 +0900

    design: Modal 스타일 변경

--- src/components/common/Modal/style.ts
+++ src/components/common/Modal/style.ts
@@ -1,3 +1,4 @@
+import { darken, lighten } from "polished";
 import styled from "styled-components";
 
 export const Background = styled.div`
@@ -11,31 +12,39 @@ export const Background = styled.div`
   z-index: 10000;
 `;
 export const Container = styled.div`
-  width: 340px;
-  height: 180px;
+  width: 350px;
+  height: 150px;
   padding: 20px 20px;
   background-color: var(--color-white);
+  letter-spacing: -1px;
 
   & h1 {
-    font-size: 2.4rem;
+    font-size: 2rem;
     font-weight: 700;
   }
 
   & p {
-    font-size: 1.8rem;
+    font-size: 1.6rem;
   }
 `;
 
 export const ButtonWrapper = styled.div`
   display: flex;
   justify-content: end;
-  margin-top: 30px;
+  margin-top: 10px;
 
   & button {
-    padding: 8px 12px;
+    padding: 8px 16px;
     background-color: var(--color-blue);
     color: var(--color-white);
     font-weight: 700;
-    font-size: 1.8rem;
+    font-size: 1.6rem;
+
+    &:hover {
+      background-color: ${lighten(0.1, "#07f")};
+    }
+    &:active {
+      background-color: ${darken(0.1, "#07f")};
+    }
   }
 `;
