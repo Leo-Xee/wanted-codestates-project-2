78649a401c4722e759c25d8a5aa086041e5000e6
Leo-Xee <jang.xmin@gmail.com>
2022-06-25 00:07:42 +0900

    feat: 유저 검색 시에 존재 하지 않는 경우 Modal 보여주는 기능 추가

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,32 +1,44 @@
-import React, { useRef } from "react";
+import React, { FormEvent, useRef, useState } from "react";
 
 import { ReactComponent as Tmi } from "@/assets/tmi_logo_default.svg";
 import { useNavigate } from "react-router-dom";
+import userService from "@/services/userService";
 import * as S from "./style";
+import Modal from "../common/Modal";
 
 function Search() {
   const navigate = useNavigate();
+  const [isModalOn, setIsModalOn] = useState(false);
   const inputRef = useRef<HTMLInputElement | null>(null);
 
-  const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (inputRef.current) {
-      const input = inputRef.current.value.replace(/ /g, "");
-      navigate(`/users/${input}`);
+      const input = inputRef.current.value.trim();
+
+      userService
+        .getUserIdByNickname(input)
+        .then(() => {
+          navigate(`/users/${input}`);
+        })
+        .catch(() => setIsModalOn(true));
     }
   };
 
   return (
-    <S.Form>
+    <S.Form onSubmit={submitHandler}>
+      {isModalOn && (
+        <Modal message="일치하는 유저가 없습니다." onClick={() => setIsModalOn(false)} />
+      )}
       <label htmlFor="searchType">
         <select id="searchType">
           <option value="유저">유저</option>
         </select>
       </label>
       <label htmlFor="search">
-        <input id="search" type="search" placeholder="카트라이더 닉네임을 입력" ref={inputRef} />
+        <input id="search" type="search" placeholder="S1법사케피를 입력해보세요." ref={inputRef} />
       </label>
-      <button type="submit" onClick={handleClick}>
+      <button type="submit">
         <Tmi />
       </button>
     </S.Form>
